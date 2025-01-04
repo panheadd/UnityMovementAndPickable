7b1737f7fc13244cf790ac6e8374ece7a20d53f0
YunusErdemGokdag <120821444+panheadd@users.noreply.github.com>
2025-01-04 19:01:29 +0300

    Created Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 currentJumpVelocity;
+    bool isJumping;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        Vector3 moveVelocity = Vector3.zero;
+        
+        moveVelocity.x = Input.GetAxis("Horizontal");
+        moveVelocity.z = Input.GetAxis("Vertical");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!isJumping)
+            {
+                isJumping = true;
+                currentJumpVelocity = Vector3.up * 5;
+            }
+
+        }
+        if(isJumping)
+        {
+            controller.Move((moveVelocity + currentJumpVelocity) * Time.deltaTime);
+            currentJumpVelocity += Physics.gravity * Time.deltaTime;
+
+            if(controller.isGrounded)
+            {
+                isJumping = false;
+            }
+        }
+        else
+        {
+            controller.SimpleMove(moveVelocity);
+        }
+    }
+}
